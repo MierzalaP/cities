f41c4faaaf4eaaed22754e63dbf6ca7386143ce9
amarghale <amarghale2@gmail.com>
2017-03-01 12:47:44 +0000

    Add city and activity form

--- client/router.js
+++ client/router.js
@@ -10,13 +10,15 @@ Router.route("/", {
 Router.route('/city/:id', {
     template: "city",
     data: function () {
-        // Return the document for the selected city (the one whose id is given)
-        // The value of this id is given by  this.params.id
+		return Cities.findOne({_id: this.params.id});	
     }
 });
 Router.route('/activity/:id', {
-    template: "city",
+    template: "activity",
     data: function () {
-
+		return Activities.findOne({_id: this.params.id});
     }
+});
+Router.route('/about', {
+	template: "about"
 });
