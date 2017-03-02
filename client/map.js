e67f7ce95fac1415ea05017368e4928c06f94657
Patrick Mierzala <patrick.mierzala@gmail.com>
2017-03-02 12:21:41 +0100

    Maps is working

---
+++ client/map.js
@@ -0,0 +1,34 @@
+if (Meteor.isClient) {
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  });
+}
+
+Template.maps.helpers({
+  mapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(this.coordinates['long'],this.coordinates['lat']),
+        zoom: 8,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false
+      };
+    }
+  }
+});
+
+Template.maps.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('maps', function(map) {
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    });
+  });
+});
